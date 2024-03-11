be281b9a41785114c1cd4784b74474430f36f4de
Amina Djarfi <m.djarfi.amina@gmail.com>
2024-03-11 23:30:45 +0100

    setup package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='LangDetectApi',
+    version='0.0.1',
+    install_requires=[
+        "flask~=2.0.2",
+        "tensorflow~=2.15.0",
+        "setuptools~=69.1.1",
+        "tqdm~=4.66.2",
+        "python-dotenv~=1.0.1",
+        "tf-keras==2.15.0"
+        "tf-slim==1.1.0"
+        "keras==2.15.0"
+
+    ],
+)
